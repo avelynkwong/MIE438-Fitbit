f8b92bd3632d4fc880fd9883ef0ecf8ade246a3a
avelynkwong <avelynkwong@gmail.com>
2024-04-02 14:17:25 -0400

    speaker test added

--- fitbit/fitbit.ino
+++ fitbit/fitbit.ino
@@ -12,6 +12,7 @@
 #include <Adafruit_ST7789.h>
 #include <SPI.h>
 #include <Wire.h>
+#include pitches.h
 
 // Display Pins
 #define TFT_CS        7  // Not used in this setup, SPI CS is used automatically
@@ -27,7 +28,7 @@ Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RESET);
 Adafruit_MPU6050 mpu;
 MAX30105 particleSensor;
 
-const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+const byte RATE_SIZE = 1; //Increase this for more averaging. 4 is good.
 byte rates[RATE_SIZE]; //Array of heart rates
 byte rateSpot = 0;
 long lastBeat = 0; //Time at which the last beat occurred
@@ -39,7 +40,7 @@ int beatAvg;
 #define QUEUE_SIZE 10
 
 // threshold for counting a step
-double UPPER_STEP_THRESH = 11.0;
+double UPPER_STEP_THRESH = 10.0;
 bool isBelowThresh = true;
 int n_steps = 0;
 
@@ -189,30 +190,41 @@ void setup(void) {
 
 void loop() {
   /* Get new sensor events with the readings */
-  long irValue = particleSensor.getIR();
+  // long irValue = particleSensor.getIR();
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-  if (checkForBeat(irValue) == true) {
-    //We sensed a beat!
-    long delta = millis() - lastBeat;
-    lastBeat = millis();
+  long irValue = particleSensor.getIR();
 
-    beatsPerMinute = 60 / (delta / 1000.0);
+  int count = 0;
+  while (count < 2){
+      if (checkForBeat(irValue) == true) {
+      //We sensed a beat!
+      long delta = millis() - lastBeat;
+      lastBeat = millis();
 
-    if (beatsPerMinute < 255 && beatsPerMinute > 20) {
-      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
-      rateSpot %= RATE_SIZE; //Wrap variable
+      beatsPerMinute = 60 / (delta / 1000.0);
 
-      //Take average of readings
-      beatAvg = 0;
-      for (byte x = 0 ; x < RATE_SIZE ; x++)
-        beatAvg += rates[x];
-      beatAvg /= RATE_SIZE;
+      // if (beatsPerMinute < 255 && beatsPerMinute > 20) {
+      //   rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+      //   rateSpot %= RATE_SIZE; //Wrap variable
+
+      //   //Take average of readings
+      //   beatAvg = 0;
+      //   for (byte x = 0 ; x < RATE_SIZE ; x++)
+      //     beatAvg += rates[x];
+      //   beatAvg /= RATE_SIZE;
+      // }
     }
+    count ++;
   }
 
   /* Print out the values */
+  Serial.print("IR=");
+  Serial.print(irValue);
+  Serial.print(", BPM=");
+  Serial.print(beatsPerMinute);
+  Serial.println("");
   
   // Fill display with black
   tft.fillScreen(ST77XX_BLACK);
@@ -224,18 +236,15 @@ void loop() {
   
   tft.println("BPM: " + String(beatsPerMinute));
   tft.println("Avg BPM: " + String(beatAvg));
-  tft.println("IR: " + String(irValue));
-  tft.println("Accel X: " + String(a.acceleration.x) + " m/s^2");
-  tft.println("Accel Y: " + String(a.acceleration.y) + " m/s^2");
-  tft.println("Accel Z: " + String(a.acceleration.z) + " m/s^2");
+  tft.println("Number of Steps: " + String(n_steps));
+  // tft.println("IR: " + String(irValue));
+  // tft.println("Accel X: " + String(a.acceleration.x) + " m/s^2");
+  // tft.println("Accel Y: " + String(a.acceleration.y) + " m/s^2");
+  // tft.println("Accel Z: " + String(a.acceleration.z) + " m/s^2");
   // tft.println("Rot X: " + String(g.gyro.x) + " m/s^2");
   // tft.println("Rot Y: " + String(g.gyro.x) + " m/s^2");
   // tft.println("Rot Z: " + String(g.gyro.x) + " m/s^2");
 
-  // Serial.print("IR=");
-  // Serial.print(irValue);
-  // Serial.print(", BPM=");
-  // Serial.print(beatsPerMinute);
   // Serial.print(", Avg BPM=");
   // Serial.print(beatAvg);
 
@@ -285,4 +294,5 @@ void loop() {
   // Serial.println(" degC");
 
   // Serial.println("");
+  // delay(50);
 }
--- heart_rate_sensor/heart_rate_sensor.ino
+++ heart_rate_sensor/heart_rate_sensor.ino
@@ -14,7 +14,7 @@ byte rateSpot = 0;
 long lastBeat = 0; //Time at which the last beat occurred
 
 float beatsPerMinute;
-int beatAvg;
+// int beatAvg;
 
 void setup() {
   Serial.begin(9600);
@@ -42,24 +42,24 @@ void loop() {
 
     beatsPerMinute = 60 / (delta / 1000.0);
 
-    if (beatsPerMinute < 255 && beatsPerMinute > 20) {
-      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
-      rateSpot %= RATE_SIZE; //Wrap variable
+    // if (beatsPerMinute < 255 && beatsPerMinute > 20) {
+    //   rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+    //   rateSpot %= RATE_SIZE; //Wrap variable
 
-      //Take average of readings
-      beatAvg = 0;
-      for (byte x = 0 ; x < RATE_SIZE ; x++)
-        beatAvg += rates[x];
-      beatAvg /= RATE_SIZE;
-    }
+    //   //Take average of readings
+    //   beatAvg = 0;
+    //   for (byte x = 0 ; x < RATE_SIZE ; x++)
+    //     beatAvg += rates[x];
+    //   beatAvg /= RATE_SIZE;
+    // }
   }
 
   Serial.print("IR=");
   Serial.print(irValue);
   Serial.print(", BPM=");
   Serial.print(beatsPerMinute);
-  Serial.print(", Avg BPM=");
-  Serial.print(beatAvg);
+  // Serial.print(", Avg BPM=");
+  // Serial.print(beatAvg);
 
   if (irValue < 50000)
     Serial.print(" No finger?");
